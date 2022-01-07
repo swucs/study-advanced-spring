3c93905557773a3120df227233e815455daae801
Hanna Park <swucs7@gmail.com>
2022-01-07 22:07:55 +0900

    로그추적기 v3

---
+++ src/main/java/spring/advanced/app/v2/OrderServiceV2.java
@@ -0,0 +1,28 @@
+package spring.advanced.app.v2;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.advanced.trace.TraceId;
+import spring.advanced.trace.TraceStatus;
+import spring.advanced.trace.hellotrace.HelloTraceV2;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    public  void orderItem(TraceId traceId, String itemId) {
+
+        TraceStatus status = null;
+
+        try {
+            status = trace.beginSync(traceId, "OrderService.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
---
+++ src/test/java/spring/advanced/trace/hellotrace/HelloTraceV2Test.java
@@ -0,0 +1,27 @@
+package spring.advanced.trace.hellotrace;
+
+import org.junit.jupiter.api.Test;
+import spring.advanced.trace.TraceStatus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTraceV2Test {
+
+    @Test
+    void begin_end_level2() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception_level2() {
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+}
